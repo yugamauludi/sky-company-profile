b7efb97c9d49243cc5bab46a2293f92b92281997
Yuga Mauludi <yugamauludi98@gmail.com>
2025-05-07 09:18:15 +0700

    chore: update configuration files for security and environment handling

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const config: NextConfig = {
   output: 'standalone',
   images: {
     unoptimized: true,
+  },
+  productionBrowserSourceMaps: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : 'http://localhost:8080/api/:path*' // Fallback URL if env variable is not set
+      }
+    ];
   }
 };
 
